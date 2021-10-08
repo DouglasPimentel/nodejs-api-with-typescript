311cfdc771009f1cfeccb745a7a4e5d644cc8bc7
DouglasPimentel <dev.pimentel@gmail.com>
2021-10-07 21:57:23 -0300

    Add initial jest configuration

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,7 @@ const root: string = path.resolve(__dirname);
 const config: Config.InitialOptions = {
   rootDir: root,
   displayName: 'tests',
-  testMatch: ['<rootDir>/test/**/*test.ts'],
+  testMatch: ['<rootDir>/test/**/*.test.ts'],
   testEnvironment: 'node',
   clearMocks: true,
   collectCoverage: true,
